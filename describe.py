cddeeb35d9d3de320d895cba834fe06cf6e3e88d
Luis <luissoto2094@gmail.com>
2025-02-22 14:59:19 +0100

    feat: add variability

--- describe.py
+++ describe.py
@@ -33,6 +33,11 @@ def percentile(data, percent):
     return data[lower] + weight * (data[upper] - data[lower])
 
 
+# Using same formula as Pandas for kurtosis
+# Kurtosis measures how extreme the outliers are (how heavy or light the tails are).
+# Kurtosis ≈ 0 → Mesokurtic (normal tails, like a normal distribution)
+# Kurtosis > 0 → Leptokurtic (heavy tails, more outliers than normal)
+# Kurtosis < 0 → Platykurtic (light tails, fewer extreme values than normal)
 def kurtosis(mean, std, len, data):
     numerator = sum(((x - mean) / std) ** 4 for x in data)
     excess_kurtosis = (
@@ -42,8 +47,11 @@ def kurtosis(mean, std, len, data):
 
 
 # Using same formula as Pandas for skewness
+# Skewness measures whether data is balanced around the mean or shifted to one side.
+# If skewness = 0 → Data is perfectly symmetric (normal distribution).
+# If skewness > 0 → Data is right-skewed (positive skew) (longer tail on the right, we have some big values to the right).
+# If skewness < 0 → Data is left-skewed (negative skew) (longer tail on the left, we have some small values to the left).
 def skewness(mean, std, len, data):
-
     return (len / ((len - 1) * (len - 2))) * sum(((x - mean) / std) ** 3 for x in data)
 
 
@@ -60,7 +68,7 @@ def get_features(filename="dataset_train.csv"):
         "max",
         "skew",
         "kurtosis",
-        # "var",
+        "var",
         "range",
     ]
     csv = get_csv(filename)
@@ -80,17 +88,26 @@ def get_features(filename="dataset_train.csv"):
             continue
         features[column]["count"] = len(summary)
         features[column]["mean"] = sum(summary) / features[column]["count"]
-        features[column]["std"] = (
+
+        # Variance and standard deviation are very similar.
+        # Variance is the average of the squared differences from the Mean.
+        # Standard deviation is the square root of the variance.
+        # STD is more commonly used because it is in the same units as the data.
+        # Variance is in squared units, which is not as intuitive, but some formulas require it, since its easier to work with.
+        features[column]["var"] = (
             sum([(x - features[column]["mean"]) ** 2 for x in summary])
             / (features[column]["count"] - 1)
-        ) ** 0.5
+        )
 
+        features[column]["std"] = features[column]["var"] ** 0.5
         sorted_summary = sorted(summary)
+
         features[column]["min"] = sorted_summary[0]
         features[column]["max"] = sorted_summary[-1]
         features[column]["25%"] = percentile(sorted_summary, 25)
         features[column]["50%"] = percentile(sorted_summary, 50)
         features[column]["75%"] = percentile(sorted_summary, 75)
+
 
         features[column]["skew"] = skewness(
             features[column]["mean"],
